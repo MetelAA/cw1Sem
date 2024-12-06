7fd0fcdc34a2e6737bd997d8fee072d2191106d7
Artem <artemmetelski78@gmail.com>
2024-12-06 03:15:17 +0300

    fgdf

--- main.c
+++ main.c
@@ -7,24 +7,29 @@ int main() {
     printf("Course work for option 5.8, created by Artem Metelskii.\n");
     int n = 0;
     scanf("%d", &n);
-    struct text text = read_text();
+    struct text text;
     switch(n){
         case 0:
+            text = read_text();
             show_text(&text);
         break;
         case 1:
+            text = read_text();
             get_metadata(&text);
             show_metadata(&text);
         break;
         case 2:
+            text = read_text();
             reverse_sentence(&text);
             show_text(&text);
         break;
         case 3:
+            text = read_text();
             sort_by_first_word(&text);
             show_text(&text);
         break;
         case 4:
+            text = read_text();
             delete_less_then_four_symbol_word_sent(&text);
             show_text(&text);
         break;
